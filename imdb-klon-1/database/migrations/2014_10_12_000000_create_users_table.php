50d5f5eb5ba3702848d361b762d1cbd5af271d43
LloydElery <dennisgooglar@gmail.com>
2024-02-12 08:42:40 +0000

    Testing

--- imdb-klon-1/database/migrations/2014_10_12_000000_create_users_table.php
+++ imdb-klon-1/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->varchar('name')->unique();
+            $table->varchar('email')->unique();
+            $table->varchar('password');
+            $table->varchar('role');
             $table->rememberToken();
             $table->timestamps();
         });
