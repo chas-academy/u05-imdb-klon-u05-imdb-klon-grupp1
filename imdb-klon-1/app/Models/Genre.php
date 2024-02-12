6e7e11607098a363e4076892eecc4af3c1b3b789
Your Name <najat_sf@hotmail.com>
2024-02-12 12:40:17 +0000

    Create review model

---
+++ imdb-klon-1/app/Models/Genre.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+// One genre has many films
+class Genre extends Model
+{
+    use HasFactory;
+    public function movies()
+    {
+        return $this->belongsToMany(Movie::class, 'genre_movies_pivot', 'genres_id', 'movies_id');
+    }
+}
+
+
+
+
+
+
+  
---
+++ imdb-klon-1/database/migrations/2024_02_12_112654_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('release_date');
+            $table->text('img_path');
+            $table->text('trailer_path');
+            $table->integer('top_rating')->default(0);
+            $table->string('movie_genres')->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
