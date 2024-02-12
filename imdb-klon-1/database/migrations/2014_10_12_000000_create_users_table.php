255c6ebae99a995f1d71a3b45bcce5de5c420190
Emilia Hårdén <emilia.harden@chasacademy.se>
2024-02-12 20:57:52 +0000

    Somehow, the database now works after getting an update from the create_users_table.

--- imdb-klon-1/database/migrations/2014_10_12_000000_create_users_table.php
+++ imdb-klon-1/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,10 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 
 return new class extends Migration
 {
@@ -14,15 +16,16 @@ return new class extends Migration
         if(!Schema::hasTable('users')){
             Schema::create('users', function (Blueprint $table) {
                 $table->id();
-                $table->varchar('name')->unique();
-                $table->varchar('email')->unique();
-                $table->varchar('password');
-                $table->varchar('role');
+                $table->string('name')->unique();
+                $table->string('email')->unique();
+                $table->string('password');
+                $table->string('role');
                 $table->rememberToken();
                 $table->timestamps();
             });
-          } 
+          }
     }
+
 
     /**
      * Reverse the migrations.
