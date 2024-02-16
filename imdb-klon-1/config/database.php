7c107a1a879f3b0344854a63199b2818e4559ade
LloydElery <dennisgooglar@gmail.com>
2024-02-16 13:49:40 +0100

    Fixed the seeder for movies table. Moving on to CategoryList. #database.php -> changed #'connections'->'strict' to false to solve an error with creating movie data with seeders.

--- imdb-klon-1/config/database.php
+++ imdb-klon-1/config/database.php
@@ -1,5 +1,6 @@
 <?php
-
+#database.php -> changed 
+#'connections'->'strict' to false to solve an error with creating movie data with seeders.
 use Illuminate\Support\Str;
 
 return [
@@ -56,7 +57,7 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
