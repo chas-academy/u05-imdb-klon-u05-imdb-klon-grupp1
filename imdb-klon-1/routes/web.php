8f40ade946931eaa50117073d906d0018aa91224
emiliaharden <emilia.harden@gmail.com>
2024-02-17 23:53:55 +0000

    Installed Breeze, fixes #44

--- imdb-klon-1/routes/web.php
+++ imdb-klon-1/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestWebController;
 use App\Models\Test;
@@ -23,6 +24,14 @@ use App\Http\Controllers\GenreController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    
+// TestController
 Route::get('/tests', [TestWebController::class, 'index']);
 
 
@@ -75,3 +84,10 @@ Route::get('/genres/{id}', [GenreController::class, 'genres.show']);
 Route::get('/genres/{id}/edit', [GenreController::class, 'genres.edit']);
 Route::put('/genres/{id}', [GenreController::class, 'genres.update']);
 Route::delete('/genres/{id}', [GenreController::class, 'genres.destroy']);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
