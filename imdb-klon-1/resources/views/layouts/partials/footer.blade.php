abee9048c63eed992110b8831cebd7e11bf48f3d
Your Name <najat_sf@hotmail.com>
2024-02-22 13:27:19 +0000

    landing page first version

--- imdb-klon-1/resources/views/layouts/partials/footer.blade.php
+++ imdb-klon-1/resources/views/layouts/partials/footer.blade.php
@@ -1,17 +1,15 @@
 <footer class="text-center text-white mt-auto">
     <div class="container pt-4">
         <section class="mb-6">
-            <!-- Social media links with Tailwind CSS classes -->
-            <a class="inline-block bg-blue-500 text-white rounded-full p-3 m-1" href="https://facebook.com" target="_blank" role="button"><i class="fab fa-facebook-f"></i></a>
-            <a class="inline-block bg-pink-500 text-white rounded-full p-3 m-1" href="https://instagram.com" target="_blank" role="button"><i class="fab fa-instagram"></i></a> 
-            <a class="inline-block bg-purple-500 text-white rounded-full p-3 m-1" href="https://twitch.com" target="_blank" role="button"><i class="fab fa-twitch"></i></a> 
-            <a class="inline-block bg-blue-400 text-white rounded-full p-3 m-1" href="https://twitter.com" target="_blank" role="button"><i class="fab fa-twitter"></i></a>
-            <a class="inline-block bg-red-600 text-white rounded-full p-3 m-1" href="https://youtube.com" target="_blank" role="button"><i class="fab fa-youtube"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-white m-1" href="https://facebook.com" target="_blank" role="button"><i class="fab fa-facebook-f"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-white m-1" href="https://instagram.com" target="_blank" role="button"><i class="fab fa-instagram"></i></a> 
+            <a class="btn btn-link btn-floating btn-lg text-white m-1" href="https://twitch.com" target="_blank" role="button"><i class="fab fa-twitch"></i></a> 
+            <a class="btn btn-link btn-floating btn-lg text-white m-1" href="https://twitter.com" target="_blank" role="button"><i class="fab fa-twitter"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-white m-1" href="https://youtube.com" target="_blank" role="button"><i class="fab fa-youtube"></i></a>
         </section>
     </div>
     
-    <!-- Copyright notice with Tailwind CSS classes -->
-    <div class="text-center py-3 bg-gray-800 text-white">
+    <div class="text-center p-3">
         &copy; 2024 by U05 IMDb-clon-Grupp1
     </div>
 </footer>
