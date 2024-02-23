c64d9d63a82fad3958c122a487255c11367c5936
Your Name <najat_sf@hotmail.com>
2024-02-23 12:29:55 +0000

    handle read() for user visitor admin, fixes #106

--- imdb-klon-1/app/Http/Controllers/DashboardadminController.php
+++ imdb-klon-1/app/Http/Controllers/DashboardadminController.php
@@ -2,17 +2,36 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Movie;
+use App\Models\Review;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class DashboardadminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+        /**
+         * Display a listing of the resource.
+         *
+         * @return \Illuminate\Http\Response
+         */
+        public function index()
+        {
+            // Authorize that the current user is an admin. If not, this might throw an exception or handle it based on your authorization logic.
+            $this->authorize('isAdmin');
+        
+            // Display all movies from the Movie model
+            $movies = Movie::all();
+        
+            // Display all users from the User model
+            $users = User::all();
+        
+            // Return the 'dashboardadmin.index' view with the retrieved data
+            return view('dashboardadmin.index', compact('movies', 'users'));
+        }
+        
+    
+
 
     /**
      * Show the form for creating a new resource.
