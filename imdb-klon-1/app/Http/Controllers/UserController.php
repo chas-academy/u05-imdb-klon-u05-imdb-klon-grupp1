23a7e5c49be2e4500fb4e2ccb858d48c24b649da
LloydElery <dennisgooglar@gmail.com>
2024-02-25 17:43:35 +0100

    Implemented CRUD for handeling users. #Added comments for each file i worked in. #Deleted files were never used #Some --resource methods were never used in the UserController, read more in comments

--- imdb-klon-1/app/Http/Controllers/UserController.php
+++ imdb-klon-1/app/Http/Controllers/UserController.php
@@ -1,4 +1,10 @@
 <?php
+#115
+#Added the following methods to the UserController:
+#->index - View all users
+#->updateUsername - Change a users username
+#->updateRole - Change a users role
+#->delete - softDelete a user from the list (they will be recoverable)
 
 namespace App\Http\Controllers;
 
@@ -12,8 +18,66 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::get();
-        return view('users', ['users' => $users]);
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
+    
+        /**
+         * Update the specified users [username] in users table.
+         */
+        public function updateUsername(Request $request, User $user)
+        {
+    
+        $request->validate([
+            'username' => 'required|string|max:255|unique:users,username,'.$user->id,
+        ]);
+
+        $user->username = $request->username;
+        $user->save();
+
+        return redirect()->route('users.index')->with('Success', 'User has been updated');
+    }
+
+    /**
+     * Update the specified users [role] in the users table.
+     */
+    public function updateRole(Request $request, User $user) 
+    {
+        $request->validate([
+            'role' => 'required|in:user,admin',
+        ]);
+
+        $user->role = $request->role;
+        $user->save();
+
+        return redirect()->route('users.index')->with('Success', 'User role has been updated!');
+    }
+
+    /**
+     * Remove the specified resource from table-view (soft delete).
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('users.index')->with('Success', 'User has been deleted!');
+    }
+
+    public function deletedUsers(User $user)
+    {
+        $user = User::withTrashed()->get();
+        //return redirect()->route('deleted.users');
+
+    }
+
+    /**
+     * Remove the specified resource from the database (force delete).
+     * 
+     * //TODO This method is never called, use as admin if you want.
+     */
+    public function forceDestroy(User $user)
+    {
+        $user->forceDelete();
+        return redirect()->route('users.index')->with('Success', 'User was permanently deleted!');
     }
 
     /**
@@ -21,7 +85,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        // This method is not needed as breeze makes it possible via other controllers.
     }
 
     /**
@@ -29,7 +93,7 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // This method is not needed as breeze makes it possible via other controllers.
     }
 
     /**
@@ -37,7 +101,7 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        
+        // This method is not needed as breeze makes it possible via other controllers.
     }
 
     /**
@@ -45,22 +109,6 @@ class UserController extends Controller
      */
     public function edit(string $id)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        // This method is not needed as breeze makes it possible via other controllers.
     }
 }
