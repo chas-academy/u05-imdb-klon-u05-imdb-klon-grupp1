7dd8df433e7377239bcb3aab35ee4ce10f758e28
Emma Jensen <emmasarahlinnea@gmail.com>
2024-02-26 18:30:30 +0000

    reapplied class w-full sm.... in guest.blade

--- imdb-klon-1/resources/views/layouts/guest.blade.php
+++ imdb-klon-1/resources/views/layouts/guest.blade.php
@@ -30,7 +30,7 @@
             </div>
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gradient-to-b from-red-950 to-zink-950 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gradient-to-b from-amber-500 to-amber-400 shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
 
