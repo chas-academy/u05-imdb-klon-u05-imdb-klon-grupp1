75b643e0bbc67636993ca68460f6c84b4da22bcf
Emma Jensen <emmasarahlinnea@gmail.com>
2024-02-26 19:01:40 +0000

    name

--- imdb-klon-1/resources/views/layouts/guest.blade.php
+++ imdb-klon-1/resources/views/layouts/guest.blade.php
@@ -19,14 +19,14 @@
 <body class="font-sans text-gray-900 antialiased">
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-red-950 dark:bg-red-950">
         <div class="flex items-center">
-            <div class="text-4xl font-bold text-orange-600 dark:text-white">
-                IMDB
+            <div class="text-4xl font-bold text-amber-300 dark:text-white">
+                ReelCorn
             </div>
-            <a href="/" class="ml-4 mr-4"> <!-- Adjust the ml and mr values as needed -->
+            <a href="/" class="ml-2 mr-2"> <!-- Adjust the ml and mr values as needed -->
                 <x-application-logo class="w-20 h-20 fill-current text-red-700" />
             </a>
-            <div class="text-4xl font-bold text-white dark:text-white">
-                CLONE
+            <div class="text-4xl font-bold text-amber-500 dark:text-white">
+                DataBase
             </div>
         </div>
 
