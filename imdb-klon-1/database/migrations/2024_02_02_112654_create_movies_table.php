24b8d373d22ee3028da170087a99a844a11f6dfd
LloydElery <dennisgooglar@gmail.com>
2024-02-26 15:48:57 +0100

    Created seederss for genres and movies table that is more in line with our final result. #There are comments at the top of each file in progress stating what changes has been made

--- imdb-klon-1/database/migrations/2024_02_02_112654_create_movies_table.php
+++ imdb-klon-1/database/migrations/2024_02_02_112654_create_movies_table.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\Review;
-use App\Models\Genre;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
--- imdb-klon-1/database/seeders/MovieSeeder.php
+++ imdb-klon-1/database/seeders/MovieSeeder.php
@@ -1,13 +1,13 @@
 <?php
-
+#118
+#Created a more realistic database that we can work with
+#Added a method that gives the movies a genre based on the id input <- this is not what we want. We want it to be based on input i think..
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use App\Models\Movie;
-use App\Models\Watchlist;
-use App\Models\Genre;
 
 class MovieSeeder extends Seeder
 {
@@ -16,16 +16,76 @@ class MovieSeeder extends Seeder
      */
     public function run(): void
     {
-        Movie::factory()->count(10)->create();
+        // Create Single movies as objects
+        $movies = [
+            [
+                'title' => 'The Shawshank Redemption',
+                'description' => 'A man is wrongfully convicted of murder and sent to prison, where he struggles to survive and maintain his hope.',
+                'release_date' => '1994-09-10',
+                'img_path' => 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEyLWVkYWUtYTI2Ni05Y2NkLWVkMjJlY29kZmM2XkEyXkFqcGdeQXVyMTQxNzMzNTI@._V1_.jpg',
+                'trailer_path' => 'https://www.youtube.com/watch?v=NmzuHjWmD6Y',
+                'top_rating' => 9.3,
+            ],
+            [
+                'title' => 'The Godfather',
+                'description' => 'A powerful mafia family struggles to maintain control over its empire.',
+                'release_date' => '1972-03-24',
+                'img_path' => 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlOWE5OWFmZjE2XkEyXkFqcGdeQXVyNzkwMjY5MDA@._V1_.jpg',
+                'trailer_path' => 'https://www.youtube.com/watch?v=sY1S3497oMA',
+                'top_rating' => 9.2,
+            ],
+            [
+                'title' => 'The Dark Knight',
+                'description' => 'Batman battles the Joker, a sadistic villain threatening to destroy Gotham City.',
+                'release_date' => '2008-07-18',
+                'img_path' => 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg',
+                'trailer_path' => 'https://www.youtube.com/watch?v=EXeTwQWrcwY',
+                'top_rating' => 9.0,
+            ],
+            [
+                'title' => 'Pulp Fiction',
+                'description' => 'Multiple stories are woven together in a violent and entertaining tale of gangsters, boxers, and criminals.',
+                'release_date' => '1994-10-14',
+                'img_path' => 'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzYwZTFiMWNkNmQ5XkEyXkFqcGdeQXVyNzkwMjY5MDA@._V1_.jpg',
+                'trailer_path' => 'https://www.youtube.com/watch?v=s7EdQ4FqbhY',
+                'top_rating' => 8.9,
+            ],
+            [
+                'title' => '12 Angry Men',
+                'description' => 'A jury must come to a decision about a man´s life in a trial where the evidence is weak.',
+                'release_date' => '1957-04-10',
+                'img_path' => 'https://m.media-amazon.com/images/M/MV5BNzBmMDkwODMtY2Q4Yi00YWU1LTg0MTctZTM4M2E1MTBmYzJlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_.jpg',
+                'trailer_path' => 'https://www.youtube.com/watch?v=G7RgN_mCIDk',
+                'top_rating' => 8.9,
+            ],
+            [
+                'title' => 'Schindler´s List',
+                'description' => 'A German businessman saves the lives of Jews during the Holocaust.',
+                'release_date' => '1993-12-15',
+                'img_path' => 'https://m.media-amazon.com/images/M/MV5BMTkzNDE3OTEwM15BMl5BanBnXkFtZTcwMzM2MTk2Mw@@._V1_.jpg',
+                'trailer_path' => 'https://www.youtube.com/watch?v=YE7VzlL7c8s',
+                'top_rating' => 8.9,
+            ],
+            [
+                'title' => 'The Lord of the Rings: The Return of the King',
+                'description' => 'Frodo and his companions journey to Mordor to destroy the One Ring.',
+                'release_date' => '2003-12-17',
+                'img_path' => 'https://m.media-amazon.com/images/M/MV5BNzA5ZDc2NDctNTY5MC00NDg2LWE4MTMtYTgwMjY5MWJhMGY0XkEyXkFqcGdeQXVyNDUyOTg4Nzg@._V1_.jpg',
+                'trailer_path' => 'https://www.youtube.com/watch?v=r5X-hFf6B94',
+                'top_rating' => 8.9,
+            ],
+        ];
 
         /**
-         * For each 'Movie'
-         * Find a random genre via its 'id'
-         * Add the genre to the movie
+         * Separate the movies into single movies
+         * creates them using the movie class 
+         * assigns the genre connection 
          */
-        foreach (Movie::all() as $movie) {
-            $genre = Genre::inRandomOrder()->take(rand(1,10))->pluck('id');
-            $movie->genres()->attach($genre);
+        foreach ($movies as $movieData) {
+            $movie = Movie::create($movieData); 
+
+            $movie->genres()->attach([1, 3]);
         }
+        
     }
 }
