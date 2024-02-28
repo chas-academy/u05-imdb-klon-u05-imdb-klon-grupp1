f2ee5d46c25040f6426fd5181717aa62307d4d71
Emma Jensen <emmasarahlinnea@gmail.com>
2024-02-28 21:09:32 +0000

    all movie grid

--- imdb-klon-1/resources/views/layouts/app.blade.php
+++ imdb-klon-1/resources/views/layouts/app.blade.php
@@ -1,64 +1,55 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
-        <title>@yield('titletab')</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <title>{{ isset($title) ? $title : config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
-        @vite('resources/css/app.css')
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-        <script src="{{ URL::asset('js/app.js') }}" type="module" defer></script>
+    <!-- Styles -->
+    @vite('resources/css/app.css')
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="{{ URL::asset('js/app.js') }}" type="module" defer></script>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-        <!-- Font Awesome -->
-        <script src="https://kit.fontawesome.com/b21a636a7a.js" crossorigin="anonymous"></script>
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/b21a636a7a.js" crossorigin="anonymous"></script>
 
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-       
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-        
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">    
-           
-        <link rel="stylesheet" href="https://bootswatch.com/5/cyborg/bootstrap.min.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 
-       
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 
-    </head>    
-    <body class="d-flex flex-column min-vh-100">
-        @include('layouts.partials.header')
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
-            
-            <!-- Page Heading -->
-            @if (isset($header))
-            <header class="bg-white dark:bg-gray-800 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-            @endif
-            
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <link rel="stylesheet" href="https://bootswatch.com/5/cyborg/bootstrap.min.css">
+</head>
+
+<body class="d-flex flex-column min-vh-100 bg-gradient-to-b from-red-950 to-zinc-950">
+    @include('layouts.partials.header')
+
+    @if (isset($header))
+    <header class="bg-white dark:bg-gray-800 shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {{ $header }}
         </div>
-        @include('layouts.partials.footer')
-    </body>
+    </header>
+    @endif
+
+    <main class="flex-grow">
+        {{ $slot }}
+    </main>
+
+    @include('layouts.partials.footer')
+</body>
 
 </html>
