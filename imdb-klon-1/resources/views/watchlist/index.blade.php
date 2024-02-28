f6fc7c03770e4ab0e08c58a7cb7d5d53d305da3a
LloydElery <dennisgooglar@gmail.com>
2024-02-28 11:17:46 +0100

    Push to have @Emma work on the feature

--- imdb-klon-1/resources/views/watchlist/index.blade.php
+++ imdb-klon-1/resources/views/watchlist/index.blade.php
@@ -1,28 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Your Watchlist</h1>
-
-    <div class="row">
-        @foreach($movies as $movie)
-        <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="{{ $movie->moviePoster() }}" alt="{{ $movie->title }}">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $movie->title }}</h5>
-                    <p class="card-text">{{ $movie->description }}</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-sm btn-outline-secondary">View</a>
-                            <!-- Add a route or form for removing from watchlist -->
-                        </div>
-                        <small class="text-muted">{{ $movie->release_date }}</small>
-                    </div>
-                </div>
-            </div>
+<div class="w-full max-h-40 overflow-x-auto"> <div class="flex flex-row -mx-4"> @foreach ($movies as $movie)
+      <div class="w-40 h-40 flex-shrink-0 mx-4 bg-white rounded-lg shadow-md overflow-hidden"> <div class="p-4">
+          <h3 class="text-lg font-semibold">{{ $movie->title }}</h3>
         </div>
-        @endforeach
-    </div>
+        <div class="p-2 bg-gray-200 rounded-lg"> <img class="object-contain w-full h-full" src="{{ $movie->img_path }}" alt="{{ $movie->title }}">
+        </div>
+      </div>
+    @endforeach
+  </div>
 </div>
 @endsection
