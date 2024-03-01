c4720d383f9410b65fc55204cc988780c3c4fd0b
Clara Prioux <prioux.clara@gmail.com>
2024-03-01 15:06:59 +0000

    fixes #183

--- imdb-klon-1/app/Models/User.php
+++ imdb-klon-1/app/Models/User.php
@@ -52,7 +52,7 @@ class User extends Model implements
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'email_verified_at' => 'datetime', // I deleted the password hashed, just problems with git commit 
     ];
 
     // One user can have one 'Watchlist'
