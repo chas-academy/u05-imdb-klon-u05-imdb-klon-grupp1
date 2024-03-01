d6e8b409632262e844d2863c444cfe4093687192
Clara Prioux <prioux.clara@gmail.com>
2024-03-01 18:15:41 +0000

    test n°3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './resources/views/**/*.blade.php', 
+    './resources/js/**/*.js', 
+  ],
   theme: {
     extend: {},
   },
