30db1d0edcd4948d1ea8bbaa802924bb63a04bde
Emma Jensen <emmasarahlinnea@gmail.com>
2024-03-03 18:03:11 +0000

    aligned movie.show divs and sizes

--- imdb-klon-1/resources/views/layouts/partials/footer.blade.php
+++ imdb-klon-1/resources/views/layouts/partials/footer.blade.php
@@ -1,15 +1,15 @@
 <footer class="text-center text-white mt-auto">
-    <div class="container pt-4">
-        <section class="mb-6">
-            <a class="btn btn-link btn-floating btn-lg text-white m-1" href="https://facebook.com" target="_blank" role="button"><i class="fab fa-facebook-f"></i></a>
-            <a class="btn btn-link btn-floating btn-lg text-white m-1" href="https://instagram.com" target="_blank" role="button"><i class="fab fa-instagram"></i></a> 
-            <a class="btn btn-link btn-floating btn-lg text-white m-1" href="https://twitch.com" target="_blank" role="button"><i class="fab fa-twitch"></i></a> 
-            <a class="btn btn-link btn-floating btn-lg text-white m-1" href="https://twitter.com" target="_blank" role="button"><i class="fab fa-twitter"></i></a>
-            <a class="btn btn-link btn-floating btn-lg text-white m-1" href="https://youtube.com" target="_blank" role="button"><i class="fab fa-youtube"></i></a>
+    <div class="container mx-auto pt-4">
+        <section class="mt-2">
+            <a class="btn btn-link btn-floating btn-lg text-white m-1 hover:text-amber-500" href="https://facebook.com" target="_blank" role="button"><i class="fab fa-facebook-f"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-white m-1 hover:text-amber-500" href="https://instagram.com" target="_blank" role="button"><i class="fab fa-instagram"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-white m-1 hover:text-amber-500" href="https://twitch.com" target="_blank" role="button"><i class="fab fa-twitch"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-white m-1 hover:text-amber-500" href="https://twitter.com" target="_blank" role="button"><i class="fab fa-twitter"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-white m-1 hover:text-amber-500" href="https://youtube.com" target="_blank" role="button"><i class="fab fa-youtube"></i></a>
         </section>
     </div>
-    
-    <div class="text-center p-3">
+
+    <div class="text-sm text-center p-3">
         &copy; 2024 by U05 IMDb-clon-Grupp1
     </div>
 </footer>
--- imdb-klon-1/resources/views/movies/show.blade.php
+++ imdb-klon-1/resources/views/movies/show.blade.php
@@ -3,45 +3,39 @@
 
 <x-app-layout>
     <div class="container card pt-2 p-3">
-        <div class="row text-center mt-3">
-            <h1 class="text-4xl">{{ $movie->title }}</h1>
+        <div class="row mt-3 flex justify-content-center items-center">
 
-            <div class="col-md mt-3">
-                @php
-                $videoId = substr($movie->trailer_path, strpos($movie->trailer_path, 'v=') + 2);
-                @endphp
-                <iframe width="100%" height="500" src="https://www.youtube.com/embed/{{ $videoId }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-        </div>
-        <div class="row text-center mt-3 justify-content-center items-center">
-            <div class="col-md flex justify-content-center items-center">
-                <img class="img-fluid" style="width: 200px" src="{{ $movie->img_path }}" alt="">
-            </div>
-            <div class="flex items-center justify-start">
-                <h2 class="text-4xl">{{ $movie->title }}</h2>
-            </div>
-            <div class="flex items-center justify-start">
-                <i class="fa-solid fa-star text-yellow-500 text-lg"></i>
-                <span class="text-lg">{{ $movie->top_rating }}</span>
-            </div>
-            <div class="flex items-start justify-start space-x-2">
-                <span class="text-lg">{{ $movie->release_date }}</span>
-                <span class="text-lg">{{ $movie->duration }}</span>
+            <img class="img-fluid" style="width: 200px" src="{{ $movie->img_path }}" alt="">
+
+
+
+
+            <div class="flex flex-col items-start justify-start ml-4">
+
+                <div class="flex items-center justify-start">
+                    <i class="fa-solid fa-star text-yellow-500 text-lg"></i>
+                    <span class="text-lg">{{ $movie->top_rating }}</span>
+                </div>
+                <p class="list-group-item text-primary">{{ $movie->genre }}</p>
+
+
+                <div class="flex items-start justify-start space-x-2">
+                    <span class="text-lg">{{ $movie->release_date }}</span>
+                    <span class="text-lg">{{ $movie->duration }}</span>
+                </div>
+                <h1 class="text-4xl mt-2">{{ $movie->title }}</h1>
+                <p>
+                    {{ $movie->description }}
+                </p>
             </div>
         </div>
 
         <div class="row mt-3">
-            <div class="col-sm">
-                <ul class="list-group list-group-horizontal flex justify-content-center items-center">
-                    <li class="list-group-item text-primary">{{ $movie->genre }}</li>
-                </ul>
-                <div class="mt-4">
-                    <p>
-                        {{ $movie->description }}
-                    </p>
-                </div>
-                <hr class="my-4">
-                <!--   <div class="mt-4 flex items-center">
+
+
+
+            <hr class="my-4">
+            <!--   <div class="mt-4 flex items-center">
                     <strong class="me-3">Directors</strong>
                     <ul class="list-group list-group-horizontal">
                         @foreach(['Carlson Young'] as $director)
@@ -58,16 +52,22 @@
                         @endforeach
                     </ul>
                 </div> -->
-            </div>
-            <div class="col-sm">
-                @auth
-                <x-primary-button class="ms-3 bg-red-950 hover:bg-orange-500">
-                    {{ __('Add To Watchlist') }}
-                </x-primary-button>
-                @endauth
-            </div>
         </div>
-        <h3 class="text-2xl">Reviews</h3>
+        <div class="col-sm">
+            @auth
+            <x-primary-button class="ms-3 bg-red-950 hover:bg-orange-500">
+                {{ __('Add To Watchlist') }}
+            </x-primary-button>
+            @endauth
+        </div>
+        <div class="col-md mt-3 flex items-center justify-center">
+            @php
+            $videoId = substr($movie->trailer_path, strpos($movie->trailer_path, 'v=') + 2);
+            @endphp
+            <iframe class="w-screen" height="500" src="https://www.youtube.com/embed/{{ $videoId }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+
+        <h3 class="text-2xl mt-4">Reviews</h3>
         @auth
         <div class="row mt-3">
 
@@ -123,5 +123,6 @@
         @else
         <h6 class="mt-4">No reviews in this movie!</h6>
         @endif
+    </div>
     </div>
 </x-app-layout>
